fe92c454b74743dafc646bf4deb804bb8576f474
Anh Lai <laingocanh1990@gmail.com>
2022-08-16 11:56:37 +0700

    add domain

---
+++ src/domain/domain.go
@@ -0,0 +1,58 @@
+package domain
+
+import (
+	"errors"
+)
+
+type CustomerRepository interface {
+	Store(customer Customer)
+	FindById(id int) Customer
+}
+
+type ItemRepository interface {
+	Store(item Item)
+	FindById(id int) Item
+}
+
+type OrderRepository interface {
+	Store(order Order)
+	FindById(id int) Order
+}
+
+type Customer struct {
+	Id int
+	Name string
+}
+
+type Item struct {
+	Id int
+	Name string
+	Value float64
+	Available bool
+}
+
+type Order struct {
+	Id int
+	Customer Customer
+	Items []Item
+}
+
+func (order *Order) Add(item Item) error {
+	if !item.Available {
+		return errors.New("cannot add unavailable items to order")
+	}
+
+	if order.value() + item.Value > 250.00 {
+		return errors.New("An order may not exceed
+		a total value of $250.00")
+	}
+	order.Items = append(order.Items, item)
+	return nil
+}
+
+func (order *Order) value() (sum float64) {
+	for i := range order.Items {
+		sum += order.Items[i].Value
+	}
+	return
+}
